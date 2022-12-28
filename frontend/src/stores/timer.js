a5f832c3eea49864505aa737cd3b19749c22b9b7
mkmktso <SpencerMAQ@users.noreply.github.com>
2022-12-28 12:29:44 +0800

    timer store (not yet tested)

---
+++ frontend/src/stores/timer.js
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
+
+export const useTimer = defineStore('useTimer', () => {
+    const secondsCountdown = ref(120)
+    // const secondsRemaining = ref(120)
+
+    const countdownString = computed(() => {
+        const minutes = Math.floor(secondsCountdown.value / 60)
+        let seconds = (secondsCountdown - minutes * 60).toString()
+        seconds = seconds.length > 1 ? seconds : `0${seconds}`
+        return `0${minutes}: ${seconds}`
+    })
+
+    let timer
+    function renewTimer() {
+        timer = setInterval(() => {
+            if (secondsCountdown.value <= 0) clearInterval(timer)
+
+            secondsCountdown.value--
+        }, 1000)
+    }
+
+    function stopTimer() {
+        clearInterval(timer)
+    }
+
+    function renewCountdownSecondsLength(seconds) {
+        secondsCountdown.value = seconds
+    }
+
+    return { secondsCountdown, renewCountdownSecondsLength, renewTimer, stopTimer, countdownString }
+})
