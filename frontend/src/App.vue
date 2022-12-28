e353e8f0779637c852fa9cec918a351b98bdb3bb
mkmktso <SpencerMAQ@users.noreply.github.com>
2022-12-28 15:19:53 +0800

    game lost modal wont show if game won, subwords reveal @ 0 timer if game won

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,6 +7,7 @@ import Header from './components/Header.vue'
 import GameLostModal from './components/modals/GameLostModal.vue'
 import SettingsModal from './components/modals/SettingsModal.vue'
 import Subwords from './components/SubWords.vue'
+import { useCurRandomWord } from './stores/currentRandomWord'
 import { useGameState } from './stores/gameState'
 import { useSettingsModal } from './stores/modalVisibility'
 import { useTimer } from './stores/timer'
@@ -15,16 +16,23 @@ const store = useSettingsModal()
 const { isSettingsModalVisible } = storeToRefs(store)
 
 const gameState = useGameState()
-const { loseState } = storeToRefs(gameState)
+const { loseState, winState } = storeToRefs(gameState)
 const { setLoseState } = gameState
 
 const timer = useTimer()
 const { secondsCountdown } = storeToRefs(timer)
 
+const randomWordStore = useCurRandomWord()
+const { revealYourSecrets } = randomWordStore
+
 watch(
     () => secondsCountdown.value,
     () => {
-        if (secondsCountdown.value <= 0) setLoseState(true)
+        if (secondsCountdown.value <= 0 && !winState) {
+            setLoseState(true)
+        } else if (secondsCountdown.value <= 0 && winState) {
+            revealYourSecrets()
+        }
     }
 )
 </script>
