a3b4498a2810af9e8d634c41602dffc6b788d780
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-01-02 00:38:36 +0800

    updated fetch URL to production URL

--- frontend/src/functions/dataFetching.js
+++ frontend/src/functions/dataFetching.js
@@ -4,7 +4,8 @@
  * @returns
  */
 export async function fetchBackendWord(gameSettings) {
-    const res = await fetch('http://127.0.0.1:5000/api/random_word', {
+    // const res = await fetch('http://127.0.0.1:5000/api/random_word', {
+    const res = await fetch('https://vue-text-twist.vercel.app/api/random_word', {
         method: 'POST',
         headers: {
             Accept: 'application/json',
