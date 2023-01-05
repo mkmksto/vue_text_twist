7f60111a333098f4ce84bba4735641eacdbd1c1e
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-01-05 19:18:44 +0800

    rebuilt vue with frontend ternary URL

--- frontend/src/functions/dataFetching.js
+++ frontend/src/functions/dataFetching.js
@@ -4,11 +4,14 @@
  * @returns
  */
 export async function fetchBackendWord(gameSettings) {
+    const fetchUrl = window.location.href.includes('vercel')
+        ? 'https://vue-text-twist.vercel.app/api/random_word'
+        : 'http://127.0.0.1:5000/api/random_word'
     // const res = await fetch('http://127.0.0.1:5000/api/random_word', {
-    const res = await fetch('https://vue-text-twist.vercel.app/api/random_word', {
+    const res = await fetch(fetchUrl, {
         method: 'POST',
         headers: {
-            'Accept': 'application/json',
+            Accept: 'application/json',
             'Access-Control-Allow-Origin': '*',
             'Content-type': 'application/json',
         },
